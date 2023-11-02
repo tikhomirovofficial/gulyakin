34284d479e186cec6dcb93128cabc32c40be7ca6
Tiks0ft <artem.tikhomirov22@gmail.com>
2023-11-02 09:27:08 +0300

    Added popup on mobile

--- src/components/AddedPopup/index.tsx
+++ src/components/AddedPopup/index.tsx
@@ -4,14 +4,15 @@ import {useAppSelector} from "../../app/hooks";
 
 const AddedPopup = () => {
     const {totalPrice, cartClassOpened, cartAdded, cartAddedPopupInfo} = useAppSelector(state => state.cart)
+    if(!cartAdded) {
+        return null
+    }
     return (
         <>
-            {
-                cartAdded ? <div className={`${styles.addedPopup}  ${cartClassOpened ? styles.addedPopupOpened : ""} p-abs f-column gap-5 t-opacity-visible-transform-3`}>
-                    <p>В корзину добавлено:</p>
-                    <b>{cartAddedPopupInfo.title}, {cartAddedPopupInfo.weight} г</b>
-                </div> : null
-            }
+            <div className={`${styles.addedPopup} ${cartClassOpened ? styles.addedPopupOpened : ""} p-abs f-column gap-5 t-opacity-visible-3`}>
+                <p>В корзину добавлено:</p>
+                <b>{cartAddedPopupInfo.title}, {cartAddedPopupInfo.weight} г</b>
+            </div>
         </>
 
     );
